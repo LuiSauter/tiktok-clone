2fe61062813bbdfd5940e8e596fe00c09f27dbd2
Luis Gabriel <janco7249@gmail.com>
2022-04-11 22:36:16 -0400

    chore(Root): Auto import React JSX

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    jsxFactory: '_jsx',
+    jsxFragment: '_jsxFragment',
+    jsxInject: `import { createElement as _jsx, Fragment as _jsxFragment } from 'react'`
+  },
   plugins: [react()]
 })
