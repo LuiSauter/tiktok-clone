d757d2df19c8ed00e536363c29acd1666f95e99b
Luis Gabriel <janco7249@gmail.com>
2022-04-12 21:32:44 -0400

    chore(Root): Change root name app for root

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles/index.css'
 
-const rootContainer = document.getElementById('app')
+const rootContainer = document.getElementById('root')
 const root = createRoot(rootContainer)
 
 root.render(
