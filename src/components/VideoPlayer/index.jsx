ed0bb732509d00334c097daf6e8d78b4995008da
Luis Gabriel <janco7249@gmail.com>
2022-04-12 21:44:20 -0400

    refactor(Root): Change contional for ternary and using clsx to classname

--- src/components/VideoPlayer/index.jsx
+++ src/components/VideoPlayer/index.jsx
@@ -1,24 +1,34 @@
-import React, { useRef, useState } from 'react'
 import styles from './styles.module.css'
+import React, { useRef, useState } from 'react'
+import clsx from 'clsx'
 
-const SRC = 'https://v16-webapp.tiktok.com/3be947bc15f984b5348f3b9e85a142c7/62552f7c/video/tos/useast2a/tos-useast2a-ve-0068c002/60704325c3f4411dabeab33738678697/?a=1988&br=3302&bt=1651&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=eXd.6HAMMyq8Z6qnhwe2Ni3oyl7Gb&l=2022041201511201022312805010A724D9&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M2lqdDU6ZmRoPDMzNzczM0ApMzNkNDwzNDw3N2U2PDNkZmdjZV9ncjRfNS9gLS1kMTZzc2A2YC1jMy81YWNfYmA0XmI6Yw%3D%3D&vl=&vr='
-
-const VideoPlayer = () => {
+const VideoPlayer = ({ src }) => {
   const [playing, setPlaying] = useState(false)
   const video = useRef()
   const handlePlay = () => {
-    if (playing) {
-      video.current.pause()
-    } else {
-      video.current.play()
-    }
+    const { current: videoEl } = video
+    playing
+      ? videoEl.pause()
+      : videoEl.play()
+
     setPlaying(!playing)
   }
 
+  const playerClassName = clsx(styles.player, {
+    [styles.hidden]: playing
+  })
+
   return (
-    <div>
-      <video ref={video} loop className={styles.video} src={SRC} controls={false} />
-      <button className={styles.player} onClick={handlePlay} />
+    <div className={styles.wrapper}>
+      <video
+        className={styles.video}
+        onClick={handlePlay}
+        src={src}
+        ref={video}
+        controls={false}
+        loop
+      />
+      <i className={playerClassName} />
     </div>
   )
 }
