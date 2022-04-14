d7758ca4d161d9f20d266467225ecb8e6450df60
Luis Gabriel <janco7249@gmail.com>
2022-04-14 13:54:43 -0400

    chore(Root): Add testing labrary with vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   esbuild: {
-    jsxFactory: '_jsx',
-    jsxFragment: '_jsxFragment',
+    jsxFactory: "_jsx",
+    jsxFragment: "_jsxFragment",
     jsxInject: `import { createElement as _jsx, Fragment as _jsxFragment } from 'react'`,
   },
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    includeSource: ["src/**/*.{js,ts}"],
+  },
 });
