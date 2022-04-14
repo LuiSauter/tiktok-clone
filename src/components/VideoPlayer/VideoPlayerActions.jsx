fc40885e664c331b26098668e10dae1f17bd8b46
Luis Gabriel <janco7249@gmail.com>
2022-04-14 14:21:04 -0400

    feat(Root): Move all icons inside src directory

--- src/components/VideoPlayer/VideoPlayerActions.jsx
+++ src/components/VideoPlayer/VideoPlayerActions.jsx
@@ -1,19 +1,22 @@
 import styles from './styles.module.css'
-import { Comment } from '../Icons/Comments'
-import { Heart } from '../Icons/Heart'
-import { Share } from '../Icons/Share'
+import { Comment } from '../../Icons/Comment'
+import { Heart } from '../../Icons/Heart'
+import { Share } from '../../Icons/Share'
 
-const VideoPlayerActions = () => {
+const VideoPlayerActions = ({ likes = 12332, comments = 122, shares = 30 }) => {
   return (
     <aside className={styles.actions}>
       <div className={styles.action}>
         <Heart width='45' />
+        <strong>{likes}</strong>
       </div>
       <div className={styles.action}>
         <Comment width='45' />
+        <strong>{comments}</strong>
       </div>
       <div className={styles.action}>
         <Share width='45' />
+        <strong>{shares}</strong>
       </div>
     </aside>
   )
