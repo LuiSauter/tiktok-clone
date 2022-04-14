c381ba1d5b528607bacf697612f74d8b98eb6cab
Luis Gabriel <janco7249@gmail.com>
2022-04-14 14:06:46 -0400

    test(Root): Setup test with vitest and Testing Library

---
+++ src/test/App.test.js
@@ -0,0 +1,15 @@
+// import React from 'react'
+import { describe, expect, it } from 'vitest'
+// import { render, screen } from '@testing-library/react'
+// import App from '../App'
+
+describe('App root', () => {
+  it('test', () => {
+    expect(true).toBe(true)
+  })
+  // it.only('test app tiktok-clone', () => {
+  //   render(<App />)
+  //   const linkElement = screen.queryByText(/Hello world/i)
+  //   expect(linkElement).toBeInTheDocument()
+  // })
+})
