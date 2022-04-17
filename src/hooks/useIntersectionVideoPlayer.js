8a36abd55328eac3ffc7b1cfe2702af03026e063
Luis Gabriel <janco7249@gmail.com>
2022-04-16 23:09:45 -0400

    feat(Root): Add intersection observer for play video automatically

---
+++ src/hooks/useIntersectionVideoPlayer.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react'
+
+const options = {
+  root: document.querySelector('main'),
+  rootMargin: '0px',
+  threshold: 0.9
+}
+
+const observer = new window.IntersectionObserver((entries) => {
+  entries
+    .forEach(entry => {
+      const { target, isIntersecting } = entry
+      target._handleIntersect(isIntersecting)
+    })
+}, options)
+
+export const useIntersectionVideoPlayer = ({ video }) => {
+  const [playing, setPlaying] = useState(false)
+
+  useEffect(() => {
+    if (!video.current) return
+    observer.observe(video.current)
+    video.current._handleIntersect = (isIntersecting) => {
+      const { current: videoEl } = video
+
+      isIntersecting
+        ? videoEl.play()
+        : videoEl.pause()
+
+      setPlaying(!videoEl.paused)
+    }
+  }, [video.current])
+
+  const handlePlay = () => {
+    const { current: videoEl } = video
+    playing
+      ? videoEl.pause()
+      : videoEl.play()
+    setPlaying(!playing)
+  }
+
+  return { handlePlay, playing }
+}
